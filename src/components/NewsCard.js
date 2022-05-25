d5b2906bd0b3e8560af8877da52005eebbda4ee0
petra-tuskan <tuskanpetra129@gmail.com>
2022-05-25 17:54:47 +0200

    modified article date

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,4 +1,6 @@
 export default function NewsCard(props) {
+  const published = `${props.publishedAt.slice(8,10)}-${props.publishedAt.slice(5,7)}-${props.publishedAt.slice(0,4)} ${props.publishedAt.slice(14,19)}`
+
   return (
     <div className="newsCard">
       <img className="newsCard__image" src={props.image_url} alt="" />
@@ -7,7 +9,7 @@ export default function NewsCard(props) {
           <h3 className="newsCard__title">{props.title}</h3>
         </a>
         <p className="newsCard__description">{props.description}</p>
-        <p className="newsCard__published">{props.publishedAt}</p>
+        <p className="newsCard__published">{published}</p>
       </div>
     </div>
   );
